58aadaea88522cd0c3b185198a51d5a24f11f2a8
Ytockai <kolia.boyackov@yandex.ru>
2025-03-20 10:50:53 +0300

    функция скачивания картинки

--- main.py
+++ main.py
@@ -3,24 +3,22 @@ from pathlib import Path
 
 Path("images").mkdir(parents=True, exist_ok=True)
 
-def list_image(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
-
-def download_image(url, file_path):
-    response = requests.get(url)
-    response.raise_for_status()
-    with open(file_path, 'wb') as file:
-        file.write(response.content)
-
-def main():
-    url = input('введите адресс картинки:')
-    numbered_list = enumerate(list_image(url)["links"]['flickr']["original"])
+def fetch_spacex_last_launch(url):
+    response_link = requests.get(url)
+    response_link.raise_for_status()
+    list_image = response_link.json()["links"]['flickr']["original"]
+    numbered_list = enumerate(list_image)
     for image in numbered_list:
         file_name = 'spacex_'+ str(image[0]) + '.jpg'
         file_path = Path("images") / file_name
-        download_image(image[1], file_path)
+        with open(file_path, 'wb') as file:
+            response = requests.get(image[1])
+            response.raise_for_status()
+            file.write(response.content)
+
+def main():
+    url = 'https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a'
+    fetch_spacex_last_launch(url)
     print('Done!')
 
 if __name__ == '__main__':
